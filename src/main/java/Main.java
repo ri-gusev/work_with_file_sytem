dbd869131ba202a6aaf57d647de4de3306a0be5f
ri-gusev <gusev.roman1@mail.ru>
2024-11-09 22:13:57 +0300

    BufferedOutputStream/BufferedInputStream

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,39 +3,19 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
-        File dir = new File("folder");
-        File file = new File (dir, "file.txt");
-        dir.mkdir();
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-//        Чтение в массив по байтам
-//        try (InputStream inputStream = new FileInputStream(file)){
-//            byte[] array = new byte[8192];
-//            int count = inputStream.read(array);
-//            StringBuilder stringBuilder = new StringBuilder();
-//            while (count > 0) {
-//                stringBuilder.append(new String(array, 0, count));
-//                count = inputStream.read(array);
-//            }
-//            System.out.println(stringBuilder.toString());
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        File dir = new File("testFolder");
+        File file = new File(dir, "f.txt");
 
-        // чтение по символьно
-        try (Reader reader = new InputStreamReader(new FileInputStream(file))){
-            int a = reader.read();
-            StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
+            long before = System.currentTimeMillis();
+            int a = inputStream.read();
+            StringBuilder result = new StringBuilder();
             while (a != -1) {
-                stringBuilder.append((char) a);
-                a = reader.read();
+                result.append((char) a);
+                a = inputStream.read();
             }
-            System.out.println(stringBuilder.toString());
-
+            long after = System.currentTimeMillis();
+            System.out.println(after - before);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/homeWork.java
+++ src/main/java/homeWork.java
@@ -1,32 +1,38 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 public class homeWork {
     public static void main(String[] args) {
-        File dir = new File("H-W");
-        File file = new File(dir, "Names.txt");
+        File dir = new File("folderHW");
+        File file = new File(dir, "fileHW");
+        ;
         dir.mkdir();
-
         try {
             file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file, true))) {
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            while (!name.equals("стоп")) {
+                outputStream.write(name.getBytes());
+                outputStream.write("\n".getBytes());
+                name = scanner.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
-            StringBuilder stringBuilder = new StringBuilder();
             int res = reader.read();
+            StringBuilder stringBuilder = new StringBuilder();
             while (res != -1) {
                 stringBuilder.append((char) res);
                 res = reader.read();
             }
-            String[] stringNames = stringBuilder.toString().split(" ");
-
-            List<String> result = Arrays.stream(stringNames)
-                    .filter((string) -> string.startsWith("А"))
-                    .toList();
-            System.out.println(result);
+            System.out.println(stringBuilder);
         } catch (Exception e) {
             e.printStackTrace();
         }
